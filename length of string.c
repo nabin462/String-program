6e81c02725df6c92658fe13d6488d07526a9d8d2
nabin462 <budhathokinabin700@gmail.com>
2026-05-25 06:27:53 +0545

    Add files via upload

---
+++ length of string.c
@@ -0,0 +1,12 @@
+//WAP to display the length of string
+#include<stdio.h>
+#include<string.h>
+int main()
+{
+	char str[20];
+	printf("enter the string\n");
+	scanf("%s",str);
+	printf("the length of string is %d",strlen(str));
+	return 0;
+	
+}
